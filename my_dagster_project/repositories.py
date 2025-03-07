1a63ffaa50f8a15ea77c35207eb85ff0062f63c6
Shrey7966 <94635069+Shrey7966@users.noreply.github.com>
2025-03-07 19:51:58 +0530

    Update repositories.py

--- my_dagster_project/repositories.py
+++ my_dagster_project/repositories.py
@@ -1,7 +1,6 @@
 from dagster import Definitions
 from my_dagster_project.my_dag import flight_price_collection, daily_schedule  # Import job and schedule
 
-# Define your jobs, schedules, and sensors
 defs = Definitions(
     jobs=[flight_price_collection],  # List your jobs
     schedules=[daily_schedule],  # List your schedules
