84e77edfdf84aee0696510e41f0a29be1aedb2dc
Shrey7966 <94635069+Shrey7966@users.noreply.github.com>
2025-03-27 19:55:56 +0530

    Update flight_price_collection_job.py
    changes made to departure date and also the path

--- my_dagster_project/flight_price_collection_job.py
+++ my_dagster_project/flight_price_collection_job.py
@@ -21,7 +21,7 @@ def fetch_flight_prices():
     fetch_date = datetime.today().strftime('%Y-%m-%d')
 
     # Depart date (when the flight is scheduled)
-    depart_date = "2025-03-28"
+    depart_date = "2025-05-31"
 
     params = {
         "fromEntityId": "BLR",
@@ -37,7 +37,7 @@ def fetch_flight_prices():
     
     # Save JSON response to S3
     s3_bucket = "flightpriceanalysisproject"
-    s3_key = f"flight_prices/{fetch_date}/{depart_date}.json" # New path format
+    s3_key = f"flight_prices_latest/{fetch_date}/{depart_date}.json" # New path format
     
     s3 = boto3.client("s3")
     s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=json.dumps(data))
